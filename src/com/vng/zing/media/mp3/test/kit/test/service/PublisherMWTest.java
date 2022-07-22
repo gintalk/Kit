5f63c706881f9cb92017839a90d8474237739ff7
namnh16 <hnam9430@gmail.com>
2022-07-22 09:21:17 +0700

    C

--- src/com/vng/zing/media/mp3/test/kit/test/service/PublisherMWTest.java
+++ src/com/vng/zing/media/mp3/test/kit/test/service/PublisherMWTest.java
@@ -5,6 +5,9 @@
 package com.vng.zing.media.mp3.test.kit.test.service;
 
 
+import com.vng.zing.common.ZErrorHelper;
+import com.vng.zing.media.commonlib.utils.CommonUtils;
+import com.vng.zing.media.commonlib.utils.ConvertUtils;
 import com.vng.zing.media.mp3.commonlib.thrift.TPublisherType;
 import com.vng.zing.media.mp3.commonlib.thrift.podcast.TPodcastEpisodeStatus;
 import com.vng.zing.media.mp3.commonlib.thrift.publisher.TPublisherAssetType;
@@ -15,19 +18,16 @@ import com.vng.zing.media.mp3.mw.publisher.thrift.req.TGetAssetSliceWithScoreMWR
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TGetFollowerCountMWReq;
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TGetFollowerSliceMWReq;
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TGetPublisherIDMWReq;
-import com.vng.zing.media.mp3.mw.publisher.thrift.req.TMGetPublisherIDMWReq;
-import com.vng.zing.media.mp3.mw.publisher.thrift.req.TMPutPublisherAliasMWReq;
-import com.vng.zing.media.mp3.mw.publisher.thrift.req.TMRemovePublisherAliasMWReq;
-import com.vng.zing.media.mp3.mw.publisher.thrift.req.TPublisherAliasExistsMWReq;
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TPutAssetMWReq;
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TPutPublisherAliasMWReq;
 import com.vng.zing.media.mp3.mw.publisher.thrift.req.TRemoveAssetMWReq;
-import com.vng.zing.media.mp3.mw.publisher.thrift.req.TRemovePublisherAliasMWReq;
+import com.vng.zing.media.mp3.mw.publisher.thrift.res.TPutPublisherAliasMWRes;
 import com.vng.zing.media.mp3.test.kit.test.common.PrintUtils;
+import org.apache.commons.io.FileUtils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @author namnh16
@@ -49,6 +49,7 @@ public class PublisherMWTest extends Test {
 //        _testRemoveFollower();
 
         _testAlias();
+//        _migrateAlias();
 
         System.exit(0);
     }
@@ -183,8 +184,8 @@ public class PublisherMWTest extends Test {
 //        ));
 
         PrintUtils.printTBase(MW.getPublisherID(new TGetPublisherIDMWReq()
-                .setPublisherType(TPublisherType.OA_ARTIST.getValue())
-                .setAliasName("apple1")
+                .setPublisherType(TPublisherType.INDIE_ARTIST.getValue())
+                .setAliasName("DT0710")
         ));
 
 //        Map<String, Integer> dataMap = new HashMap<>();
@@ -204,5 +205,44 @@ public class PublisherMWTest extends Test {
 //                .setPublisherType(TPublisherType.ARTIST.getValue())
 //                .setAliasNames(Arrays.asList("rubyourcamel", "dontrubyourcamel"))
 //        ));
+    }
+
+    private static void _migrateAlias() {
+        try {
+            List<String> lines = FileUtils.readLines(new File("data/indie_artist.csv"), StandardCharsets.UTF_8);
+
+            for (String line : lines) {
+                String[] split = line.split(",");
+                if (split.length != 2) {
+                    System.err.println("Invalid line: " + line);
+                    continue;
+                }
+
+                int artistID = ConvertUtils.toInteger(split[0]);
+                if (artistID <= 0) {
+                    System.err.println("Invalid artist ID: " + line + " " + split[0]);
+                    continue;
+                }
+
+                String alias = split[1];
+                if (CommonUtils.isEmpty(alias)) {
+                    System.err.println("Invalid alias: " + line);
+                    continue;
+                }
+
+                TPutPublisherAliasMWRes res = TZMP3PublisherMWClient.INST.putPublisherAlias(new TPutPublisherAliasMWReq()
+                        .setPublisherType(TPublisherType.INDIE_ARTIST.getValue())
+                        .setAliasName(alias)
+                        .setPublisherID(artistID)
+                );
+                if (ZErrorHelper.isFail(res.error)) {
+                    System.err.println("Error: " + res.error + " " + alias + " " + artistID);
+                }
+            }
+
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
     }
 }
