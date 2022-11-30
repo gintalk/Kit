846f046ce5266c4255499dc7ff9d5a467790b54b
namnh16 <hnam9430@gmail.com>
2022-11-30 11:35:43 +0700

    Y

--- src/com/vng/zing/media/mp3/test/kit/test/service/UserTest.java
+++ src/com/vng/zing/media/mp3/test/kit/test/service/UserTest.java
@@ -7,14 +7,44 @@ package com.vng.zing.media.mp3.test.kit.test.service;
  * @author namnh16 on 26/04/2021
  */
 
-import org.apache.commons.codec.digest.DigestUtils;
+import com.vng.zing.media.commonlib.utils.WhiteListUserIdUtils;
+import com.vng.zing.media.mp3.commonlib.thrift.TUserListType;
+import com.vng.zing.media.mp3.service.user.thrift.client.TZMP3UserServiceClient;
+import com.vng.zing.media.mp3.service.user.thrift.req.TGetAssetSliceWithScoreReq;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UserTest extends Test {
 
+    private static final TZMP3UserServiceClient USER_SERVICE = TZMP3UserServiceClient.INST;
+
     public static void main(String[] args) {
-        String a = "ab6761610000e5eb1e3ded354accca81933429d7";
-        String b = "ab676161000051741e3ded354accca81933429d7";
-        String c = "ab6761610000f1781e3ded354accca81933429d7";
-        String d = "ab676186000010164c015961fbfd8a0928f28f10";
+        List<Integer> userIDs = Arrays.asList(
+//                WhiteListUserIdUtils.KHAITQ2_USERID,
+//                WhiteListUserIdUtils.KHOIDD,
+//                WhiteListUserIdUtils.TAMNQT_USERID,
+//                WhiteListUserIdUtils.TRUONGDT,
+//                WhiteListUserIdUtils.NAMNH16,
+                1023635027
+        );
+
+//        System.out.println(USER_SERVICE.multiAddAssets(new TMultiAddAssetsReq()
+//                .setTime(System.currentTimeMillis())
+//                .setAssetId(1091572488)
+//                .setUserIds(userIDs)
+//                .setAssetType(TUserListType.LIBRARY_PLAYLIST.getValue())
+//        ));
+
+        for (Integer userID : userIDs) {
+            System.out.println(USER_SERVICE.getAssetSliceWithScore(new TGetAssetSliceWithScoreReq()
+                    .setUserId(userID)
+                    .setStart(0)
+                    .setCount(200)
+                    .setAssetType(TUserListType.LIBRARY_PLAYLIST.getValue())
+            ).value.values);
+        }
+
+        System.exit(0);
     }
 }
